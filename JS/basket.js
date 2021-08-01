0e1050f2ae3b575ef75ba3d7eaff11a9f2e2a83d
Tony B <dev@difiworld.com>
2021-08-02 01:53:50 +0200

    - recupération du panier complet - mise en forme de l'affichage du panier - intégration du formulaire pour l identite de l acheteur - intégration des emplacements pour les montants TTC HT TVA

--- JS/basket.js
+++ JS/basket.js
@@ -1,12 +1,51 @@
 function tableOfProduct(){
-    for(var i = 0; i < localStorage.length; i++) {
-        var productBuy      =       localStorage.getItem[i];
-        console.log(productBuy);
-        // productBuy.forEach(element => console.log(element));
 
-        
+    let cart        =       JSON.parse(localStorage.getItem("cart"));
+    
+    for(var product in cart){
+        const cartP                                 =       cart[product]
 
-    }
+        let productLine                            =       document.createElement('div');
+            productLine.classList.add("row") ;
+            productLine.classList.add("margeProduct") ;
+            productLine.classList.add("borderL") ;
+
+            let productLineImage                        =       document.createElement('div');
+                productLineImage.classList.add("col-2") ;
+                productLineImage.classList.add("centerFull") ;
+                productLineImage.innerHTML              =       '<img src="' + cartP.imageUrl + '" class="card-img-top" alt="' + cartP.cameraname + '"/ >';
+
+            let productLineBloc                         =       document.createElement('div');
+                productLineBloc.classList.add("col-8") ;
+                productLineImage.classList.add("centerFull") ;
+                let productLineBlocTitle                =       document.createElement('p');
+                    productLineBlocTitle.classList.add("Dark")
+                    productLineBlocTitle.innerHTML      =       '<a href="../pages/detailProduits.html?_id='+ cartP.id + '">"' + cartP.cameraname + ' - avec l\'objectif ' + cartP.selectedLens+ '</a>' ;
+                let productLineBlocText                 =       document.createElement('p');
+                    productLineBlocText.innerHTML       =       'Quantité : ' + cartP.quantity;
+                let productLineBlocAction               =       document.createElement('p');
+                    productLineBlocAction.innerHTML     =       '<button id="deleteProduct" type="button" class="btn btn-warning">Supprimer l\'article</button>';
+
+
+            let productLinePrice                        =       document.createElement('div');
+                productLinePrice.classList.add("col-2") ;
+                productLineImage.classList.add("centerFull") ;
+            let productLinePriceText                    =       document.createElement('p');
+                let LinePrice                           =       (Number(cartP.price)/100)*cartP.quantity
+                productLinePriceText.innerHTML              =       LinePrice.toLocaleString("EUR", {style: "currency", currency: "EUR"});
+                
+            
+        TOPB__List.appendChild(productLine);
+            productLine.appendChild(productLineImage);
+            productLine.appendChild(productLineBloc);
+                productLineBloc.appendChild(productLineBlocTitle);
+                productLineBloc.appendChild(productLineBlocText);
+                productLineBloc.appendChild(productLineBlocAction);
+            productLine.appendChild(productLinePrice);
+                productLinePrice.appendChild(productLinePriceText);
+
+
+    };
 
 
 
