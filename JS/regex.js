70c1e69ed945a084a2346820a4731d36fd9f723e
Tony B <dev@difiworld.com>
2021-08-11 16:37:04 +0200

    - debug de l'ajout au panier
    - modification des regex
    
    - différentes tentatives pour utiliser les classes JS avec le localstorage et
        l'affichage de la liste des Cameras

--- JS/regex.js
+++ JS/regex.js
@@ -2,7 +2,7 @@
     
     // Définition des REGEX
     const regexZipCode      =    new RegExp('^[0-9]{5}', 'i');
-    const regexCity         =    new RegExp('^[a-zA-Z]\-\ ')   ;
+    // const regexCity         =    new RegExp('^[a-zA-Z]\-\ ')   ;
     const regexTel          =    new RegExp('^0[1-9]([-. ]?[0-9]{2}){4}$', 'i') ;
     const regexEmail        =    new RegExp('^([a-zA-Z0-9_-])+([.]?[a-zA-Z0-9_-]{1,})*@([a-zA-Z0-9-_]{2,}[.])+[a-zA-Z]{2,}\\s*$', 'i') ;
     
@@ -14,7 +14,6 @@
     
         // Récupération des champs de message de validation
     let divZipCode      =    document.querySelector('#verifZipCode');
-    let divCity         =    document.querySelector('#verifCity');
     let divTel          =    document.querySelector('#verifTel');
     let divEmail        =    document.querySelector('#verifEmail');
       
@@ -42,23 +41,23 @@
         }); 
 
             // champs Ville
-        selectCity.addEventListener('input',() => {
-            if(selectCity.value.match(regexCity)){
-                divCity.classList.remove('hidden')
-                divCity.classList.remove('text-danger')
-                divCity.classList.add('text-success')
-                divCity.classList.add('font-weight-bold')
-                divCity.innerHTML =   '<em><i class="fas fa-check"></i> Ville correcte</em>'
-                buttonValid.removeAttribute("disabled");
-            }else{
-                divCity.classList.remove('hidden')
-                divCity.classList.remove('text-success')
-                divCity.classList.add('text-danger')
-                divCity.classList.add('font-weight-bold')
-                divCity.innerHTML =   '<p><i class="fas fa-times"></i> Ville incorrecte</p>'
-                buttonValid.setAttribute("disabled", "");
-            }
-        }); 
+        // selectCity.addEventListener('change',() => {
+        //     if(selectCity.value.match(regexCity)){
+        //         divCity.classList.remove('hidden')
+        //         divCity.classList.remove('text-danger')
+        //         divCity.classList.add('text-success')
+        //         divCity.classList.add('font-weight-bold')
+        //         divCity.innerHTML =   '<em><i class="fas fa-check"></i> Ville correcte</em>'
+        //         buttonValid.removeAttribute("disabled");
+        //     }else{
+        //         divCity.classList.remove('hidden')
+        //         divCity.classList.remove('text-success')
+        //         divCity.classList.add('text-danger')
+        //         divCity.classList.add('font-weight-bold')
+        //         divCity.innerHTML =   '<p><i class="fas fa-times"></i> Ville incorrecte</p>'
+        //         buttonValid.setAttribute("disabled", "");
+        //     }
+        // }); 
             // champs téléphone
         inputTel.addEventListener('input',() => {
             if(inputTel.value.match(regexTel)){
